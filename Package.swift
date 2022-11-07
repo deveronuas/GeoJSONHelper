523b95924634bc0491074c835b58a31d15290c61
Asheshp23 <asheshp23@gmail.com>
2022-11-07 11:24:26 -0500

    refactored codebase

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "GeoJSONHelper",
+      name: "GeoJSONSwiftHelper",
       dependencies: [
         .product(name: "Turf", package: "turf-swift"),
         .product(name: "GEOSwift", package: "GEOSwift")
