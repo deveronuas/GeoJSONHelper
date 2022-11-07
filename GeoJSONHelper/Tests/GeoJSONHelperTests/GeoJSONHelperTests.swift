706a5073ff2351a821e06de2dfe154ba05375d07
Asheshp23 <asheshp23@gmail.com>
2022-11-07 10:07:27 -0500

    Added dependancies

---
+++ GeoJSONHelper/Tests/GeoJSONHelperTests/GeoJSONHelperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GeoJSONHelper
+
+final class GeoJSONHelperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GeoJSONHelper().text, "Hello, World!")
+    }
+}
